0c737af7c7bf562470345f8462677139cac2ba6e
JMH <jheld135@gmail.com>
2017-05-31 19:44:57 -0500

    Add zsh alias: sme -> source-map-explorer

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,8 @@ alias asrch="alias | grep"
 # Reload this config file
 alias rzsh="source ~/.zshrc"
 
+alias sme="source-map-explorer"
+
 for alias_file in $SH_CONFIG_DIR/aliases/*.zsh
 do
     source $alias_file
